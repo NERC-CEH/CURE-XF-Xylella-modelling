396df348a73bd10817b5126318f950274eecb811
dchap79 <dcha@ceh.ac.uk>
2018-09-24 13:38:20 +0100

    Update 1 - Shiny SEIR model.R

--- 1 - Shiny SEIR model.R
+++ 1 - Shiny SEIR model.R
@@ -21,6 +21,7 @@ SEIR_model = function(initial_infection=0.001,
                       latent_period_yrs = 1,
                       yrs_to_death_E = 200,
                       yrs_to_death_I = 20,
+                      vaccination_rate = 0.1,
                       number_of_years = 20,
                       plot=TRUE){
   # Solve continuous SEIR with latent infections
@@ -31,31 +32,37 @@ SEIR_model = function(initial_infection=0.001,
   seir <- function(time, state, parameters) {
     require(deSolve)
     with(as.list(c(state, parameters)), {
-      dS <- -beta*S*I - betaL*S*E
-      dE <- beta*S*I + betaL*S*E - sigma*E
+      dS <- -beta*S*I - betaL*S*E - S*v
+      dE <- beta*S*I + betaL*S*E - sigma*E - gammaL*E
       dI <- sigma*E - gamma*I
-      dR <-  gamma*I
-      return(list(c(dS, dE, dI, dR)))
+      dR <-  gamma*I + gammaL*E
+      dV <- S*v
+      return(list(c(dS, dE, dI, dR, dV)))
     })
   }
   
   ## Initial proportion in each compartment
-  init <- c(S = 1-initial_infection, E = initial_infection, I=0.0, R = 0.0)
+  init <- c(S = 1-initial_infection, E = initial_infection, I=0.0, R = 0.0, V=0.0)
   ## parameter list (beta: infection parameter; gamma: recovery parameter)
   parameters <- c(beta = transmission_rate_I, betaL=transmission_rate_E, 
                   gamma=1/yrs_to_death_I, gammaL=1/yrs_to_death_E,
-                  sigma=1/latent_period_yrs)
+                  sigma=1/latent_period_yrs,
+                  v = vaccination_rate)
   ## Time frame
   times <- seq(0, number_of_years, by = 0.1)
   
   ## Solve using ode (General Solver for Ordinary Differential Equations)
   out <- as.data.frame(ode(y = init, times = times, func = seir, parms = parameters))
-  out$diseased = 1-out$S
+  out$uninfected = out$S + out$V
+  out$diseased = 1 - out$S - out$V
   
   if(plot){
     df = melt(out, id.vars="time")
-    print(ggplot(df, aes(x=time, y=value, colour=variable)) +
+    myPalette = c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7","#999999")[c(7,3,6,2,4,8,5)]
+    print(ggplot(df, aes(x=time, y=value, colour=variable, shape=variable)) +
             geom_line(lwd=1) +
+            geom_point(data=df[df$time%%2==0,], inherit.aes=TRUE, size=3) +
+            scale_colour_manual(values=myPalette) +
             ylab("Proportion of population") +
             xlab("Years since infection") +
             theme(legend.title = element_blank()))
@@ -63,7 +70,6 @@ SEIR_model = function(initial_infection=0.001,
   
   return(out)
 }
-
 
 
 ui <- fluidPage(
@@ -74,42 +80,47 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-    
+      
       h4("Settings for asymptomatic infecteds"),
-               
+      
       sliderInput(inputId="transmission_rate_E",
-                           label="Transmission rate",
-                           value=0.5, min=0, max=6.0, step=0.1),
-               
-      sliderInput(inputId="yrs_to_death_E",
-                           label="Average time to death (years)",
-                           value=200, min=0.1, max=200.0, step=0.1),
-               
+                  label="Transmission rate",
+                  value=0.5, min=0, max=6.0, step=0.1),
+      
+      sliderInput(inputId="mortality_E",
+                  label="Mortality per year",
+                  value=0, min=0, max=1, step=0.01),
+      
       sliderInput(inputId="latent_period_yrs",
-                           label="Average length of asymptomatic period (years)",
-                           value=1, min=0.1, max=3.0, step=0.1),
-    
+                  label="Average length of asymptomatic period (years)",
+                  value=1, min=0.1, max=3.0, step=0.1),
+      
       h4("Settings for symptomatic infecteds"),
-    
+      
       sliderInput(inputId="transmission_rate_I",
-              label="Transmission rate",
-              value=2, min=0, max=6.0, step=0.1),
-    
-      sliderInput(inputId="yrs_to_death_I",
-                label="Average time to death (years)",
-                value=5, min=0.1, max=10.0, step=0.1),
-    
+                  label="Transmission rate",
+                  value=3, min=0, max=10, step=0.1),
+      
+      sliderInput(inputId="mortality_I",
+                  label="Mortality per year",
+                  value=0.2, min=0, max=1, step=0.01),
+      
       h4("General settings"),
-    
+      
+      sliderInput(inputId="vaccination_rate",
+                  label="Vaccination rate",
+                  value=0, min=0, max=1, step=0.01),
+      
       sliderInput(inputId="number_of_years",
-                label="Number of years",
-                value=25, min=1, max=100, step=1)
+                  label="Number of years",
+                  value=20, min=1, max=100, step=1)
     ),
-  
+    
     # *Output() functions
     mainPanel(tabsetPanel(
-      tabPanel("Plot", plotOutput(outputId = "infection_plot", height = "700px", width = "900px")),
-      tabPanel("Data", 
+      tabPanel("Simple plot", plotOutput(outputId = "simple_plot")),
+      tabPanel("Detailed plot", plotOutput(outputId = "infection_plot")),
+      tabPanel("Model output", 
                downloadButton("downloadData", "Download"),
                tableOutput(outputId = "data_table") )
     ))
@@ -122,31 +133,67 @@ server <- function(input, output){
     SEIR_model( transmission_rate_E = input$transmission_rate_E,
                 transmission_rate_I = input$transmission_rate_I,
                 latent_period_yrs = input$latent_period_yrs,
-                yrs_to_death_E = input$yrs_to_death_E,
-                yrs_to_death_I = input$yrs_to_death_I,
+                yrs_to_death_E = 1/input$mortality_E,
+                yrs_to_death_I = 1/input$mortality_I,
+                vaccination_rate = input$vaccination_rate,
                 number_of_years = input$number_of_years,
                 plot=FALSE)
   })
   
   output$infection_plot <- renderPlot(expr={ 
-    df = melt(sim(), id.vars="time")
-    print(ggplot(df, aes(x=time, y=value, colour=variable)) +
+    out = sim()
+    out = out[,!(names(out) %in% c("uninfected", "diseased"))]
+    df = melt(out, id.vars="time")
+    df$label = factor(c("Susceptible", "Infected (asymptomatic)", "Infected (symptomatic)", 
+                        "Dead", "Vaccinated")[as.numeric(df$variable)],
+                      levels=c("Susceptible", "Infected (asymptomatic)", "Infected (symptomatic)", 
+                               "Dead", "Vaccinated"))
+    myPalette = c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", 
+                  "#D55E00", "#CC79A7","#999999")[c(7,3,6,2,4)]
+    print(ggplot(df, aes(x=time, y=value, colour=label, shape=label)) +
             geom_line(lwd=1) +
-            ylab("Proportion of population") +
+            geom_point(data=df[df$time%%2==0,], inherit.aes=TRUE, size=5) +
+            scale_colour_manual(values=myPalette) +
+            ylab("Proportion of olive trees") +
             xlab("Years since infection") +
-            theme(legend.position = "top",
+            theme(legend.position = "bottom",
                   legend.title = element_blank(),
                   legend.text = element_text(size=20),
                   axis.title = element_text(size=20),
-                  axis.text = element_text(size=12)))
-    }) 
+                  axis.text = element_text(size=12),
+                  legend.key.size = unit(1, "cm") )
+          )
+  }) 
+  
+  output$simple_plot <- renderPlot(expr={ 
+    out = sim()
+    out = out[,names(out) %in% c("time", "uninfected", "diseased")]
+    df = melt(out, id.vars="time")
+    df$label = factor(c("Uninfected", "Diseased")[as.numeric(df$variable)],
+                      levels=c("Uninfected", "Diseased"))
+    myPalette = c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", 
+                  "#D55E00", "#CC79A7","#999999")[c(3,7)]
+    print(ggplot(df, aes(x=time, y=value, colour=label, shape=label)) +
+            geom_line(lwd=1) +
+            scale_colour_manual(values=myPalette) +
+            ylab("Proportion of olive trees") +
+            xlab("Years since infection") +
+            theme(legend.position = "bottom",
+                  legend.title = element_blank(),
+                  legend.text = element_text(size=20),
+                  axis.title = element_text(size=20),
+                  axis.text = element_text(size=12),
+                  legend.key.size = unit(1, "cm") )
+    )
+  }) 
   
   output$data_table <- renderTable({ 
     df = sim()
     names(df)[1] = "Year"
-    df = df[df$Year %% 1 == 0,]}, digits=3 
-    )
-
+    df = df[df$Year %% 1 == 0,]}, 
+    digits=3 
+  )
+  
   output$downloadData <- downloadHandler(
     filename = paste0("SEIR_dynamics-", Sys.time(), ".csv"),
     content = function(con) {
